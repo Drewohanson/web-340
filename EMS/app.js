15e141d5a79de44f30fa5891061087b36daa4c3a
Drew Hanson <drew.o.hanson@gmail.com>
2019-04-13 21:51:18 -0600

    adding week 8

--- EMS/app.js
+++ EMS/app.js
@@ -18,11 +18,20 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
-var mongoose = require("mongoose");
 var Employee = require('./models/employee');
+var helmet = require("helmet");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+
+
+// setup csrf protection
+var csrfProtection = csrf({cookie: true});
 
 //mlab and mongoose connection
 var mongoDB = "mongodb+srv://Drewohanson:<password>@cluster0-7zbdb.mongodb.net/test?retryWrites=true";
+
 mongoose.connect(mongoDB, {
   useNewUrlParser: true
 });
@@ -43,7 +52,20 @@ app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "ejs");
 
+//use statements
 app.use(logger("short"));
+app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+  var token = req.csrfToken();
+  res.cookie('XSRF-TOKEN', token);
+  res.locals.csrfToken = token;
+  next();
+});
 
 //model
 var employee = new Employee({
@@ -53,10 +75,22 @@ var employee = new Employee({
 
 app.get("/", function(request, response) {
   response.render("index", {
-    title: "Home page"
+    title: "Home page",
+    message: "XSS Prevention Example"
   });
 });
 
+app.get('/new', function(req, res) {
+  res.render('new', {
+    title: 'New Entry',
+    message: "New Employee Entry Page"
+  });
+})
+
+app.post("/process", function(req,res) {
+  console.log(req.body.txtName);
+  res.redirect("/");
+});
 //create server
 http.createServer(app).listen(8080, function() {
   console.log("Application started on port 8080!");
