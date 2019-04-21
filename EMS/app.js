733cde1fc031f4b4485db524ee77992410727ccd
Drew Hanson <drew.o.hanson@gmail.com>
2019-04-20 22:19:01 -0600

    pushing week 9

--- EMS/app.js
+++ EMS/app.js
@@ -92,7 +92,7 @@ app.post("/process", function(req,res) {
   res.redirect("/");
 });
 //create server
-http.createServer(app).listen(8080, function() {
-  console.log("Application started on port 8080!");
+http.createServer(app).listen(app.get("port"), function() {
+  console.log("Application started on port " + app.get("port"));
 });
 //End Program
